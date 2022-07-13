ca198b45f8b3b9b5a94f2064cd018b3fc6ff4746
Supersonic2510 <57788560+Supersonic2510@users.noreply.github.com>
2022-07-13 17:06:35 +0200

    Update uninstall.sh

--- ASO-F1/uninstall.sh
+++ ASO-F1/uninstall.sh
@@ -10,15 +10,6 @@ then
 	#statements
 	sudo rmmod LED1.ko
 	sudo rmmod LED2.ko
-	#Move pushbutton 1
-	sudo mv /usr/lib/lkm/pushbutton1.sh ./
-	#Move pushbutton 2
-	sudo mv /usr/lib/lkm/pushbutton2.sh ./
-	#Move pushbutton 3
-	sudo mv /usr/lib/lkm/pushbutton3.sh ./
-	#Move pushbutton 4
-	sudo mv /usr/lib/lkm/pushbutton4.sh ./
-	sudo rm -rf /usr/lib/lkm
 	echo "LED module has succsefully been uninstalled!"
 else
 	#statements
