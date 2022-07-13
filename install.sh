ddd7ce1d988a1c6cbe89a908b5502f0086478f06
Supersonic2510 <57788560+Supersonic2510@users.noreply.github.com>
2022-07-13 17:05:37 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,9 @@ sudo groupadd mediagroup
 sudo usermod -a -G mediagroup root
 sudo usermod -a -G groupname $USER
 sudo rm -rf /media/music
-sudo cp -a ./music/ /media/music/
+sudo cp -R ./music /media/music/
 sudo rm -rf /etc/apache2/
-sudo cp -a ./apache2/ /etc/apache2/
+sudo cp -R ./apache2 /etc/apache2/
+sudo chgrp -R mediagroup /media/music/
+sudo chmod -R 777 /media/music
 sudo bash /media/music/startPlaylist.sh
